8f39191e2889290bbf38277ee65601482531b5c3
Dawid <dpytka@gmail.com>
2010-05-03 17:40:06 +0200

    layout

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,18 +1,4 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 Ext.onReady(function(){
-
-  var win = new Ext.Window({
-    width: 500,
-    height: 300,
-    closeAction: 'hide',
-    buttons: [{
-      text: "Zamknij",
-			onClick: function() {
-				win.close();
-			}
-    }]
-  });
-  
-  win.show();
 });
