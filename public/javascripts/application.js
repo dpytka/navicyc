921c870f50b3c55a9213e320957a1510c6f20885
Dawid <dpytka@gmail.com>
2010-05-03 18:14:57 +0200

    cd

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,45 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 Ext.onReady(function(){
+
+  Ext.QuickTips.init();
+  
+  var api = new ApiPanel();
+  var mainPanel = new MainPanel();
+  
+  api.on('click', function(node, e){
+    if (node.isLeaf()) {
+      e.stopEvent();
+      mainPanel.loadClass(node.attributes.href, node.id);
+    }
+  });
+  
+  mainPanel.on('tabchange', function(tp, tab){
+    api.selectClass(tab.cclass);
+  });
+  
+  var viewport = new Ext.Viewport({
+    layout: 'border',
+    items: [{
+      cls: 'docs-header',
+      height: 36,
+      region: 'north',
+      xtype: 'box',
+      el: 'header',
+      border: false,
+      margins: '0 0 5 0'
+    }, api, mainPanel]
+  });
+  
+  api.expandPath('/root/apidocs');
+  
+  // allow for link in
+  var page = window.location.href.split('?')[1];
+  if (page) {
+    var ps = Ext.urlDecode(page);
+    var cls = ps['class'];
+    mainPanel.loadClass('output/' + cls + '.html', cls, ps.member);
+  }
+  
+  viewport.doLayout();  
 });
--- public/javascripts/docs.js
+++ public/javascripts/docs.js
@@ -89,91 +89,15 @@ DocPanel = Ext.extend(Ext.Panel, {
     this.title = ps[ps.length - 1];
     Ext.apply(this, {
       tbar: ['->', {
-        text: 'Config Options',
-        handler: this.scrollToMember.createDelegate(this, ['configs']),
-        iconCls: 'icon-config'
-      }, '-', {
-        text: 'Properties',
-        handler: this.scrollToMember.createDelegate(this, ['props']),
-        iconCls: 'icon-prop'
-      }, '-', {
-        text: 'Methods',
-        handler: this.scrollToMember.createDelegate(this, ['methods']),
-        iconCls: 'icon-method'
-      }, '-', {
-        text: 'Events',
-        handler: this.scrollToMember.createDelegate(this, ['events']),
-        iconCls: 'icon-event'
-      }, '-', {
-        text: 'Direct Link',
-        handler: this.directLink,
-        scope: this,
-        iconCls: 'icon-fav'
-      }, '-', {
-        tooltip: 'Hide Inherited Members',
-        iconCls: 'icon-hide-inherited',
-        enableToggle: true,
-        scope: this,
-        toggleHandler: function(b, pressed){
-          this.body[pressed ? 'addClass' : 'removeClass']('hide-inherited');
-        }
-      }, '-', {
-        tooltip: 'Expand All Members',
-        iconCls: 'icon-expand-members',
-        enableToggle: true,
-        scope: this,
-        toggleHandler: function(b, pressed){
-          this.body[pressed ? 'addClass' : 'removeClass']('full-details');
-        }
+        text: 'Options'
       }]
     });
     DocPanel.superclass.initComponent.call(this);
-  },
-  
-  directLink: function(){
-    var link = String.format("<a href=\"{0}\" target=\"_blank\">{0}</a>", document.location.href + '?class=' + this.cclass);
-    Ext.Msg.alert('Direct Link to ' + this.cclass, link);
-  },
-  
-  scrollToMember: function(member){
-    var el = Ext.fly(this.cclass + '-' + member);
-    if (el) {
-      var top = (el.getOffsetsTo(this.body)[1]) + this.body.dom.scrollTop;
-      this.body.scrollTo('top', top - 25, {
-        duration: 0.75,
-        callback: this.hlMember.createDelegate(this, [member])
-      });
-    }
-  },
-  
-  scrollToSection: function(id){
-    var el = Ext.getDom(id);
-    if (el) {
-      var top = (Ext.fly(el).getOffsetsTo(this.body)[1]) + this.body.dom.scrollTop;
-      this.body.scrollTo('top', top - 25, {
-        duration: 0.5,
-        callback: function(){
-          Ext.fly(el).next('h2').pause(0.2).highlight('#8DB2E3', {
-            attr: 'color'
-          });
-        }
-      });
-    }
-  },
-  
-  hlMember: function(member){
-    var el = Ext.fly(this.cclass + '-' + member);
-    if (el) {
-      if (tr = el.up('tr')) {
-        tr.highlight('#cadaf9');
-      }
-    }
   }
 });
 
 
 MainPanel = function(){
-
   this.searchStore = new Ext.data.Store({
     proxy: new Ext.data.ScriptTagProxy({
       url: 'http://extjs.com/playpen/api.php'
@@ -247,48 +171,4 @@ Ext.extend(MainPanel, Ext.TabPanel, {
       this.setActiveTab(p);
     }
   }
-});
-
-Ext.onReady(function(){
-
-  Ext.QuickTips.init();
-  
-  var api = new ApiPanel();
-  var mainPanel = new MainPanel();
-  
-  api.on('click', function(node, e){
-    if (node.isLeaf()) {
-      e.stopEvent();
-      mainPanel.loadClass(node.attributes.href, node.id);
-    }
-  });
-  
-  mainPanel.on('tabchange', function(tp, tab){
-    api.selectClass(tab.cclass);
-  });
-  
-  var viewport = new Ext.Viewport({
-    layout: 'border',
-    items: [{
-      cls: 'docs-header',
-      height: 36,
-      region: 'north',
-      xtype: 'box',
-      el: 'header',
-      border: false,
-      margins: '0 0 5 0'
-    }, api, mainPanel]
-  });
-  
-  api.expandPath('/root/apidocs');
-  
-  // allow for link in
-  var page = window.location.href.split('?')[1];
-  if (page) {
-    var ps = Ext.urlDecode(page);
-    var cls = ps['class'];
-    mainPanel.loadClass('output/' + cls + '.html', cls, ps.member);
-  }
-  
-  viewport.doLayout();  
 });
