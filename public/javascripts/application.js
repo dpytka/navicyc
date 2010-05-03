8df60eb410347fdbaa028f3fa6785aecfcd3e44d
Dawid <dpytka@gmail.com>
2010-05-03 08:02:53 +0200

    Extjs resources + application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+Ext.onReady(function(){
+
+  var win = new Ext.Window({
+    width: 500,
+    height: 300,
+    closeAction: 'hide',
+    buttons: [{
+      text: "Zamknij",
+			onClick: function() {
+				win.close();
+			}
+    }]
+  });
+  
+  win.show();
+});
