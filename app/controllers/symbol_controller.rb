1d2a804c2d7d0df8871003f4459528576725608c
Dawid <dpytka@gmail.com>
2010-05-15 19:45:39 +0200

    params String to symbol

--- app/controllers/symbol_controller.rb
+++ app/controllers/symbol_controller.rb
@@ -1,8 +1,8 @@
 require 'cycr'
 
 class SymbolController < ApplicationController
-  def show 
+  def show
     cyc = Cyc::Client.new
-    @all_genls = cyc.comment :Dog
+    @all_genls = cyc.comment params[:name].to_sym
   end
 end
