31a5e9a20b5305b1e7e84d7f4b0ceaee5bbbf140
Dawid <dpytka@gmail.com>
2010-05-15 23:33:49 +0200

    improvements in javascripts

--- public/javascripts/DocPanel.js
+++ public/javascripts/DocPanel.js
@@ -1,12 +1,12 @@
-DocPanel = Ext.extend(Ext.Panel, {
-  closable: true,
+var DocPanel = Ext.extend(Ext.Panel, {
+    closable: true,
 
-  initComponent: function() {
-    Ext.apply(this, {
-      tbar: ['->', {
-               text: 'Options'
-             }]
-    });
-    DocPanel.superclass.initComponent.call(this);
-  }
+    initComponent: function() {
+        Ext.apply(this, {
+            tbar: ['->', {
+                text: 'Options'
+            }]
+        });
+        DocPanel.superclass.initComponent.call(this);
+    }
 });
--- public/javascripts/SearchPanel.js
+++ public/javascripts/SearchPanel.js
@@ -1,35 +1,37 @@
-SearchPanel = function() {
-  SearchPanel.superclass.constructor.call(this, {
-    id: 'api-tree',
-    region: 'west',
-    split: true,
-    header: false,
-    width: 195,
-    minSize: 175,
-    maxSize: 500,
-    collapsible: true,
-    margins: '0 0 5 5',
-    cmargins: '0 0 0 0',
-    collapseMode: 'mini'
-  });
+var SearchPanel = function() {
+    SearchPanel.superclass.constructor.call(this, {
+        id: 'api-tree',
+        region: 'west',
+        split: true,
+        header: false,
+        width: 195,
+        minSize: 175,
+        maxSize: 500,
+        collapsible: true,
+        margins: '0 0 5 5',
+        cmargins: '0 0 0 0',
+        collapseMode: 'mini'
+    });
 };
 
 Ext.extend(SearchPanel, Ext.grid.GridPanel, {
-  initComponent: function() {
-    Ext.apply(this, {
-      store: new Ext.data.ArrayStore({
-        fields: ['name']
-      }),
-      columns: [{
-                  header: 'Name',
-                  dataIndex: 'name'
-                }],
-      tbar: [' ', new Ext.form.TextField({
-               width: 150,
-               emptyText: 'Search...',
-               enableKeyEvents: true
-             })]
-    })
-    SearchPanel.superclass.initComponent.call(this);
-  }
+    initComponent: function() {
+        Ext.apply(this, {
+            store: new Ext.data.ArrayStore({
+                fields: ['name']
+            }),
+            columns: [
+                {
+                    header: 'Name',
+                    dataIndex: 'name'
+                }
+            ],
+            tbar: [' ', new Ext.form.TextField({
+                width: 150,
+                emptyText: 'Search...',
+                enableKeyEvents: true
+            })]
+        });
+        SearchPanel.superclass.initComponent.call(this);
+    }
 });
