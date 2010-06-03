d3d8355b636b4d56e44a50977accc502f45ca0b6
Dawid <dpytka@gmail.com>
2010-06-03 19:08:12 +0200

    helpers for symbol html

--- app/helpers/symbol_helper.rb
+++ app/helpers/symbol_helper.rb
@@ -1,2 +1,13 @@
 module SymbolHelper
+  def format_comment(comment)
+    comment.gsub(/#\$[\w|-]+/) { |symbol| "<a href=\"#\">#{symbol[2..-1]}</a>" }
+  end
+
+  def format_all_genls(all_genls)
+    return_string = ''
+    for genls in all_genls
+      return_string += "<a href=\"#\">#{genls}</a> "
+    end
+    return_string
+  end
 end
